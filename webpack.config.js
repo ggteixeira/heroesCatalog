cd862d566a96dc8a9a1dbffaa1274ec834e3811d
Guilherme <guiemi.teixeira@gmail.com>
2020-07-21 20:00:42 -0300

    Sets up manually the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 module.exports = {
-   // define entry file and output
-   entry: './src/index.js',
-   output: {
-       path: path.resolve('dist'),
-       filename: 'main.js'
-   },
-   // define babel loader
-   module: {
-       rules: [
-           { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
-       ]
-   }
+    // define entry file and output
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'main.js'
+    },
+    // define babel loader
+    module: {
+        rules: [
+            { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    }
 };
 
