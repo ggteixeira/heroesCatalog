b1ec634a81de240e5eafdf0d1cb2077f81be59ac
Guilherme <guiemi.teixeira@gmail.com>
2020-07-22 20:39:52 -0300

    Puts all views into the Views folder (just created btw)

---
+++ src/Views/Catalog.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+function Catalog(){
+    return (
+        <h1 className="text-center text-2xl">This is the catalog</h1>
+
+        <div>
+            
+
+        </div>
+    )
+}
+
+export default Catalog
+
