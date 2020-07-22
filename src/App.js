13e62a66b19a4bcf7b5272085c7c6789a2ee2662
Guilherme <guiemi.teixeira@gmail.com>
2020-07-21 21:35:57 -0300

    Renames App.js and switches from arrow function to standard function

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss'
+
+function App () {
+    return (
+        <div className="container">
+        <h1>Hello World, mano!</h1>
+    </div>
+
+    )
+}
+
+export default App;
