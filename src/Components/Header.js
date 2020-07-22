19508260fccb989c4416926b409e54bac4a1bfe0
Guilherme <guiemi.teixeira@gmail.com>
2020-07-21 22:17:31 -0300

    Installs Tailwindcss for easy styling and responsivity

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(){
+    return (
+        <header className="border-b font-bold p-3">
+            Heroes Catalog
+        </header>
+    )
+}
+
+export default Header
