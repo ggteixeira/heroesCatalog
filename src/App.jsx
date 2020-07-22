339901659009109f3fdb8cf54690f1cdd56e1866
Guilherme <guiemi.teixeira@gmail.com>
2020-07-21 21:27:43 -0300

    Sets up manually the project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss'
+
+const App = () => ( // transformar numa function normal depois, p/ serguir o padrão
+    <div className="container">
+        <h1>Hello World, React!</h1>
+    </div>
+)
+
+export default App;
