21087404b1aa2c970b45ec282100609f7225bab4
Guilherme <guiemi.teixeira@gmail.com>
2020-07-23 12:25:36 -0300

    Adds custom colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'black-t-50': 'rgba(0, 0, 0, 0.5)',
+        'white-printi': '#ffffff',
+        'gray-printi': '#eeeeee',
+        'blue-printi': '#017ccc',
+        'blue-printi-transparent': 'rgba(1, 122, 203, 0.5)',
+        'yellow-printi': '#ffd334'
+      },
+      colors: {
+        'blue-printi': '#017ccc',
+      }
+    },
   },
   variants: {},
   plugins: [],
