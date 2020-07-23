1f86d4c1cef75256ea95db1d0128aff484ea5a66
Guilherme <guiemi.teixeira@gmail.com>
2020-07-22 23:30:07 -0300

    FETCHES DATA FROM MARVEL API <3 <3 <3

--- src/Views/Catalog.js
+++ src/Views/Catalog.js
@@ -1,15 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
-function Catalog(){
+// APIs keys
+const timeStamp = '1595465427'
+const apiKey = '4d83457e38505c349716636eea8d24d5' // public key
+const md5 = '33ffba8f09b558e72e11ab67e2f80ba4'
+
+// base URL: `https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}`
+
+
+function Catalog() {
+    const [heroes, setHeroes] = useState([])
+    
+    useEffect(() => {
+
+        // axios({
+        //     method: 'GET',
+        //     url: `https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}`
+        // }).then(res => {
+        //     setHeroes(res.name)
+        // })
+        axios
+        .get(`https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}`)
+        .then(response => {
+            console.log(response)
+        })
+        .catch( err => console.log(err))
+
+        // Set State
+    })
+
     return (
-        <h1 className="text-center text-2xl">This is the catalog</h1>
-
-        <div>
-            
-
-        </div>
-    )
+        <h1>Heroes Page</h1>
+        )
 }
 
 export default Catalog
