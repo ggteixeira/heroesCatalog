e49f33d56052c8cbc1cce10606baed682aafb06c
Guilherme <guiemi.teixeira@gmail.com>
2020-07-24 19:24:39 -0300

    Fixes footer position

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return(
-        <footer className="bg-gray-printi text-center text-xs p-5 absolute bottom-0 w-full">
+        <footer className="bg-gray-printi text-center text-xs p-3 absolute bottom-0 w-full">
             &copy; Printi 2020
         </footer>
     )
