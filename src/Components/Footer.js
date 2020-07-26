dd313c8d6df073d33d27650547a5c86a13ed39d5
Guilherme <guiemi.teixeira@gmail.com>
2020-07-26 18:46:22 -0300

    Fixes linting

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Footer() {
-    return(
+    return (
         <footer className="bg-gray-printi text-center text-xs p-3 fixed bottom-0 w-full">
             &copy; Printi 2020
         </footer>
