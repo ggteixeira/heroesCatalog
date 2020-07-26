1633fbc712c5a396fd4b643535a8d299e98a7564
Guilherme <guiemi.teixeira@gmail.com>
2020-07-26 19:48:45 -0300

    WIP: a commit before trying to mirror repo

--- src/Views/Catalog.js
+++ src/Views/Catalog.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 // import axios from 'axios'
 import { useParams, Link } from 'react-router-dom'
+import { useHistory } from 'react-router'
 
 // APIs keys
 const timeStamp = '1595465427'
@@ -9,6 +10,7 @@ const md5 = '33ffba8f09b558e72e11ab67e2f80ba4'
 
 function Catalog() {
   const [characterInfos, setCharacterInfos] = useState([])
+  const [characterId, setCharacterId] = useState([])
 
   // console.log(`https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&orderBy=name&limit=100`)
 
@@ -19,34 +21,24 @@ function Catalog() {
       .then(data => data.data.results)
       .then(data => setCharacterInfos(data))
 
-  }, [])
+  }, [characterInfos])
+
   return (
+    // <div><p>Olá, mundo</p></div>
     <div className="w-full flex flex-wrap space-x-2 justify-start">
       {characterInfos.map(hero => (
-        <div 
-        className="bg-gray-500 max-w-sm m-3 rounded overflow-hidden shadow-lg"
-        >
-          
-
-
+        <div className="bg-gray-500 max-w-sm m-3 rounded overflow-hidden shadow-lg">
           <img className="h-48 w-full" src={hero.thumbnail.path + '/standard_xlarge.jpg'} alt="Sunset in the mountains"></img>
-          
             <div class="font-bold text-sm m-2">
-          
               <p className="text-center">{hero.name}</p>
-              
-          
-            </div>
-            <p className="text-center hover:bg-gray-600 border border-gray-400 px-4 py-2 text-gray-800"><Link to="/details">Details</Link></p>
+            </div>          
+            <p className="hover:bg-gray-600 border text-center border-gray-400 px-4 py-2 text-gray-800"><Link to="/details">Click for more details</Link></p>
           </div>
 
       ))}
     </div>
   )
 }
-
-// className="flex justify-between flex-wrap"
-{/* <img className="w-full" src={hero.thumbnail.path + '/portrait_medium.jpg'}></img> */}
 
 export default Catalog
 
